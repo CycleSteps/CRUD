74f743a75bb875dc02770ce162d2fbf215ae0fb4
CycleSteps <168162574+CycleSteps@users.noreply.github.com>
2024-10-15 16:13:45 +0530

    CRUD CHANGES

--- project/app/urls.py
+++ project/app/urls.py
@@ -5,4 +5,6 @@ from app import views
 urlpatterns = [
     path('',views.index,name="index"),
     path('insert', views.insertData,name="insertData"),
+    path('update/<id>', views.updateData,name="updateData"),
+    path('delete/<id>', views.deleteData,name="deleteData"),
 ]
--- project/app/views.py
+++ project/app/views.py
@@ -1,10 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Student
+from django.contrib import messages
 
 # Create your views here.
+
+
 def index(request):
-    return render(request,"index.html")
+    data=Student.objects.all()
+    print(data)
+    context={"data":data}
+    return render(request,"index.html",context)
+
+
+
+
+
 def insertData(request):
+    
     if request.method == "POST":
         studentId = request.POST.get("studentId")
         studentName = request.POST.get("studentName")
@@ -14,6 +26,40 @@ def insertData(request):
         print(studentId,studentName,gender,studentAge,studentAddress)
         query=Student(studentId=studentId,studentName=studentName,gender=gender,studentAge=studentAge,studentAddress=studentAddress)
         query.save()
-        return render(request,"index.html",{"message":"Data inserted successfully"})
+        messages.info(request,"Data inserted Successfully")
+        return redirect("/")
+        # return render(request,"index.html",{"message":"Data inserted successfully"})
+    
+def updateData(request,id):
+    
+    if request.method == "POST":
         
-    return render(request,"index.html")
+        studentId = request.POST["studentId"]
+        studentName = request.POST["studentName"]
+        gender = request.POST["gender"]
+        studentAge = request.POST["studentAge"]
+        studentAddress = request.POST["studentAddress"]
+        
+        edit=Student.objects.get(id=id)
+        edit.studentId = studentId
+        edit.studentName=studentName
+        edit.gender=gender
+        edit.studentAge=studentAge
+        edit.studentAddress=studentAddress
+        edit.save()
+        messages.warning(request,"Data updated Successfully")
+    
+        return redirect("/")
+    d=Student.objects.get(id=id)
+    context={"d":d}
+    return render(request,"edit.html",context)
+
+def deleteData(request,id):
+    d=Student.objects.get(id=id)
+    d.delete()
+    messages.error(request,"Data deleted Successfully")
+    return redirect("/")
+    # context={"data":data}
+    # return render(request,"index.html",context)
+        
+    # return render(request,"index.html") 
